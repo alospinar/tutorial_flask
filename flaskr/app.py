0a3e20115358b44b89c3370c16bc5fd82d3d7e99
Andres Leonardo Ospina Rios <al.ospinar1@uniandes.edu.co>
2026-01-27 12:29:01 -0500

    implementacion de asociaciones

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,27 @@
 from flaskr import create_app
+from .modelos import db, Cancion, Medio, Album, Usuario
 
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+#PRUEBA
+
+with app.app_context():
+    u=Usuario(nombre='admin',contrasena='12345')
+    a=Album(titulo='Album1',anio=2020, descripcion='texto',medio=Medio.CD)
+    c=Cancion(titulo='mi cancion', minutos=2,segundos=23,interprete='yo')
+    u.albumes.append(a)
+    a.canciones.append(c)
+    db.session.add(u)
+    db.session.add(c)
+    db.session.commit()
+    print(Album.query.all())
+    print(Album.query.all()[0].canciones)
+    print(Cancion.query.all())
+    db.session.delete(a)
+    print(Album.query.all())
+    print(Cancion.query.all())
---
+++ flaskr/modelos/modelos.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+import enum
+db = SQLAlchemy()
+
+albumes_canciones = db.Table('album_canciones',\
+    db.Column('album_id',db.Integer,db.ForeignKey('album.id'), primary_key=True),\
+    db.Column('cancion_id',db.Integer, db.ForeignKey('cancion.id'),primary_key=True))
+
+class Cancion(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    titulo = db.Column(db.String(128))
+    minutos = db.Column(db.Integer)
+    segundos = db.Column(db.Integer)
+    interprete = db.Column(db.String(128))
+    albumes = db.relationship('Album', secondary='album_canciones',back_populates='canciones')
+
+class Medio(enum.Enum):
+    DISCO = 1
+    CASETE = 2
+    CD = 3
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    titulo = db.Column(db.String(128))
+    anio = db.Column(db.Integer)
+    descripcion = db.Column(db.String(256))
+    medio = db.Column(db.Enum(Medio))
+    usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    canciones = db.relationship('Cancion', secondary= 'album_canciones', back_populates= 'albumes')
+    __table_args__ = (db.UniqueConstraint('usuario','titulo', name='titulo_unico_album'),)
+    
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(64))
+    contrasena = db.Column(db.String(32))
+    albumes = db.relationship('Album',cascade = "all, delete, delete-orphan")
+    
