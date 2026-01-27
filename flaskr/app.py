eabc21b40f9c7fbbae0f36a446e8f3ade92b1214
Andres Leonardo Ospina Rios <al.ospinar1@uniandes.edu.co>
2026-01-27 13:32:32 -0500

    implementacion de serializacion de clases

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,6 @@
 from flaskr import create_app
 from .modelos import db, Cancion, Medio, Album, Usuario
-
+from .modelos import AlbumSchema,CancionSchema, UsuarioSchema
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
@@ -11,17 +11,8 @@ db.create_all()
 #PRUEBA
 
 with app.app_context():
-    u=Usuario(nombre='admin',contrasena='12345')
-    a=Album(titulo='Album1',anio=2020, descripcion='texto',medio=Medio.CD)
-    c=Cancion(titulo='mi cancion', minutos=2,segundos=23,interprete='yo')
-    u.albumes.append(a)
-    a.canciones.append(c)
-    db.session.add(u)
+    c = Cancion(titulo="Prueba", minutos=2, segundos=20, interprete="Sultanito")
+    c_schema = CancionSchema()
     db.session.add(c)
     db.session.commit()
-    print(Album.query.all())
-    print(Album.query.all()[0].canciones)
-    print(Cancion.query.all())
-    db.session.delete(a)
-    print(Album.query.all())
-    print(Cancion.query.all())
+    print([c_schema.dump(c) for c in Cancion.query.all()])
