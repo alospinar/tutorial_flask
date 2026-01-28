1245334bf31d3017f5c0cff0ac2dfba9028f0e21
Andres Leonardo Ospina Rios <al.ospinar1@uniandes.edu.co>
2026-01-27 22:53:46 -0500

    CRUD funcionalidades

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,12 +1,25 @@
 from flaskr import create_app
 from .modelos import db, Cancion, Medio, Album, Usuario
 from .modelos import AlbumSchema,CancionSchema, UsuarioSchema
+from flask_restful import Api
+from .vistas import VistaCanciones, VistaCancion, VistasSignIn, VistaLogIn, VistaAlbumsUsuario,VistaAlbum, VistaCancionesAlbum
+
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
 
 db.init_app(app)
 db.create_all()
+
+api = Api(app)
+api.add_resource(VistaCanciones, '/canciones')
+api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
+api.add_resource(VistasSignIn, '/signin')
+api.add_resource(VistaLogIn, '/login')
+api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
+api.add_resource(VistaAlbum, '/album/<int:id_album>')
+api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
+
 
 #PRUEBA
 
